d7fa4ae5f738458ecd795f3989836ef845be8253
Indra Setyajiwirawan Guritno <indrasetyajiwirawanguritno@gmail.com>
2019-07-17 11:10:04 +0700

    glasses setup

--- glasses.ino
+++ glasses.ino
@@ -8,3 +8,14 @@ SW_I2C u8x8(/* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
 String string;
 char data;
 
+void setup(){ 
+  BTserial.begin(38400);
+  
+  u8x8.begin();
+  u8x8.setPowerSave(0);
+
+  u8x8.setFont(u8x8_font_artossans8_u);
+  u8x8.draw2x2String(1,3,"WELCOME");
+  delay(5000);
+  u8x8.clearDisplay();
+}
